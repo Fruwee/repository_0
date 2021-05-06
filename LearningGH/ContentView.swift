004d606f8d1b50949429ab85487e9fd961c5184b
Fruwee <petrbenes25@gmail.com>
2021-05-06 19:34:15 +0200

    hello what

---
+++ LearningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGH
+//
+//  Created by Petr on 02.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Hhhhhhhhssss")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
